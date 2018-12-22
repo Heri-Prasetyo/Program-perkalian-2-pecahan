b9b5ffd9b55fd84719483d1602cd903ce9a98197
Heri Prasetyo <45659342+Heri-Prasetyo@users.noreply.github.com>
2018-12-22 10:29:08 +0700

    Add files via upload

---
+++ Program C++ Perkalian 2 Pecahan by Heri 2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+int p1,p2,Hasil;
+
+cout<<"PROGRAM PERKALIAN 2 PECAHAN"<<endl;
+
+cout<<"Masukan Pecahan Pertama = ";
+cin>>p1;
+cout<<"Masukan Pecahan Kedua   = ";
+cin>>p2;
+Hasil=p1*p2;
+cout<<"Hasilnya Perkalian 2 Bilangan Pecahan = "<<Hasil;
+return 0;
+
+
+
+
+
+
+
+}
